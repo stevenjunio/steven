9212de06894ada61dc116fb4e2ca4ae3c782b3d9
Steven Junio <Steven.junio91@gmail.com>
2024-02-27 07:40:13 -0800

    navmenu

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Blog() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+      <div>blog</div>
     </main>
   );
 }
