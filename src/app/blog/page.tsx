3f31be2cd0747eedd5f4833c15d873799ff22223
Steven Junio <Steven.junio91@gmail.com>
2024-02-27 20:34:42 -0800

    blog is here

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function Blog() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>blog</div>
+      <div>blog is here</div>
     </main>
   );
 }
