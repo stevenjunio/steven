730a65d372d5e573115c653f356fdaa8b37d03fd
Steven Junio <Steven.junio91@gmail.com>
2024-03-08 13:31:24 -0800

    Ok fixed?

--- apps/steven-junio/next.config.mjs
+++ apps/steven-junio/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+const __dirname = path.resolve(path.dirname(''));
+console.log(`dirname`, path.join(__dirname, "../.."));
 const nextConfig = {
+    experimental: {
+        ...(process.env.NODE_ENV === "development"
+            ? { outputFileTracingRoot: path.join(__dirname, "../..") }
+            : null),
+    },
     images: {
         remotePatterns: [
             {
