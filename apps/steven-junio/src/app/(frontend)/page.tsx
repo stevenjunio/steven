bc44237947ef793c13e8e6f29098bb5240a8a403
Steven Junio <Steven.junio91@gmail.com>
2024-03-11 21:15:35 -0700

    Remove bad page

--- apps/steven-junio/src/app/(frontend)/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Welcome to steven-junio!</h1>
-      <p>Learn more about the project at https://nx.dev</p>
-    </div>
-  );
-}
--- apps/steven-junio/src/app/page.tsx
+++ apps/steven-junio/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <HideableHeader />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div></div>
+        <div>hello home</div>
       </main>
     </>
   );
