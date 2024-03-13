542784fa459d65f06c0f38375adffa7615541eeb
Steven Junio <Steven.junio91@gmail.com>
2024-03-12 21:05:51 -0700

    Change link

--- apps/steven-junio/src/app/(admin)/admin/components/admin-dashboard.tsx
+++ apps/steven-junio/src/app/(admin)/admin/components/admin-dashboard.tsx
@@ -58,7 +58,7 @@ export async function AdminDashboard() {
       <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-[60px] items-center border-b px-6">
-            <Link className="flex items-center gap-2 font-semibold" href="#">
+            <Link className="flex items-center gap-2 font-semibold" href="/">
               <Package2Icon className="h-6 w-6" />
               <span className="">Acme Inc</span>
             </Link>
@@ -111,7 +111,7 @@ export async function AdminDashboard() {
       </div>
       <div className="flex flex-col">
         <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-          <Link className="lg:hidden" href="#">
+          <Link className="lg:hidden" href="/">
             <Package2Icon className="h-6 w-6" />
             <span className="sr-only">Home</span>
           </Link>
