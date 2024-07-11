849e54bdb84aae2311000d1c23711c7d7cea897a
Steven Junio <Steven.junio91@gmail.com>
2024-07-11 15:06:18 -0700

    chore: Update package.json and layout.tsx, add theme styles

--- apps/steven-junio/src/app/components/TimeOfDayScene.tsx
+++ apps/steven-junio/src/app/components/TimeOfDayScene.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import {
-  Sphere,
-  Sky,
-  OrbitControls,
-  Cloud,
-  PerspectiveCamera,
-} from "@react-three/drei";
-import { Suspense, useEffect, useState } from "react";
-import { AmbientLight, Vector3 } from "three";
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import Clouds from "./skyScene/Clouds";
 import Scene from "./skyScene/MainScene";
 
 const TimeOfDayScene = () => {
   const [loading, setLoading] = useState(true);
+  const handleContextMenu = useCallback((event: any) => {
+    event.preventDefault();
+    console.log(`right click on canvas`);
+  }, []);
   return (
     <div className="h-full h-90vh">
       {loading ? (
@@ -24,11 +20,10 @@ const TimeOfDayScene = () => {
           className="bg-gradient-to-b from-blue-300 to-gray-400 h-screen w-full fixed z-50 top-0"
         />
       ) : null}
-      <Canvas>
+      <Canvas onContextMenu={handleContextMenu}>
         <PerspectiveCamera makeDefault position={[0, 0, 10]} />
         <Scene
           onLoadingChange={() => {
-            console.log(`finally loaded`);
             setLoading(false);
           }}
         />
