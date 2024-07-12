6727710de9c9f38a2946261ec9100d5af4f62f82
Steven Junio <Steven.junio91@gmail.com>
2024-07-12 16:51:59 -0700

    feat: Add new project "StevenJunio.com" with updated description and technologies

--- apps/steven-junio/data/projects.ts
+++ apps/steven-junio/data/projects.ts
@@ -1,24 +1,62 @@
 export const projects = [
   {
+    slug: "this",
+    title: "StevenJunio.com",
+    image: "/images/steven-junio-screenshot.webp",
+    description: "This site",
+    technologies: ["React", "Next.js", "Xano"],
+  },
+  {
     slug: "syllabus",
     title: "Syllabus",
     image: "/images/syllabus-screenshot.webp",
     description: "A modern SEO analysis tool",
-  },
-  {
-    slug: "my-daily-workouts-co",
-    title: "MyDailyWorkoutsCo",
-    image: "/images/MyDailyWorkoutsCo-screenshot.webp",
-    description: "Automated workout generator",
-    technologies: ["Bubble.io", "Javascript"],
+    longDescription:
+      "Syllabus is a modern SEO analysis tool that helps you understand how your website is performing in search engines. It provides you with a detailed report of your website's SEO performance, including keyword rankings, backlinks, and more.",
+    technologies: [
+      "React",
+      "Next.js",
+      "Tailwind CSS",
+      "Node.js",
+      "TypeScript",
+      "PostgreSQL",
+      "Docker",
+      "Cloudflare",
+      "Railway",
+      "Playwright",
+      "Bubble.io",
+      "n8n",
+    ],
+    liveUrl: "https://syllabus.io",
+    myRole: "Senior Software Developer",
+    roleDescription:
+      "My time was split 60/40 between front-end and back-end development. I was responsible for major performance overhaul projects, on the front-end and backend. I built out major features that allowed users to deeply analyze webpage content using PIXI.js as the rendering engine. \nI implemented continuous testing utilizing Playwright. I build custom infrastructure using docker and Railway to deploy scalable testing shards to allow instant, parallel testing of webpages. \n I also built out major infrastructure changes to pull a dockerized version of n8n with workers to allow for scalable, parallelized workflows.",
+    startDate: new Date("September 2022"),
   },
   {
     slug: "outskirts",
     title: "Outskirts",
     image: "/images/outskirts-screenshot.webp",
     description: "Marketplace for Agriculture equipment",
-    technologies: ["Bubble.io", "Javascript"],
+    technologies: ["Bubble.io", "Javascript", "HTML", "CSS", "node.js"],
   },
+  {
+    slug: "tubey-the-tubeman",
+    title: "Tubey The Tubeman",
+    image: "/images/tubey-the-tubeman.webp",
+    video:
+      "https://files.stevenjunio.com/Tubey%202.0%20Website%20%20Video_FINAL_50%20mbps.mp4",
+    description: "The original Mini Wacky Wavy Inflatable Arm Flailing Tubeman",
+    technologies: ["Shopify"],
+  },
+  {
+    slug: "my-daily-workouts-co",
+    title: "MyDailyWorkoutsCo",
+    image: "/images/MyDailyWorkoutsCo-screenshot.webp",
+    description: "Automated workout generator",
+    technologies: ["Bubble.io", "Javascript", "HTML", "CSS"],
+  },
+
   {
     slug: "electrified",
     title: "Electrified",
@@ -41,22 +79,7 @@ export const projects = [
     description: "Plugin that displays images/files without uploading to aws",
     technologies: ["Javascript", "HTML", "CSS"],
   },
-  {
-    slug: "tubey-the-tubeman",
-    title: "Tubey The Tubeman",
-    image: "/images/tubey-the-tubeman.webp",
-    video:
-      "https://files.stevenjunio.com/Tubey%202.0%20Website%20%20Video_FINAL_50%20mbps.mp4",
-    description: "The original Mini Wacky Wavy Inflatable Arm Flailing Tubeman",
-    technologies: ["Shopify"],
-  },
-  {
-    slug: "steven-junio-com",
-    title: "StevenJunio.com",
-    image: "/images/steven-junio-screenshot.webp",
-    description: "This site",
-    technologies: ["React", "Next.js", "Xano"],
-  },
+
   {
     slug: "bubble-random-generator-plugin",
     title: "Bubble Random Generator Plugin",
---
+++ apps/steven-junio/src/app/(frontend)/projects/[slug]/page.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  Heading,
+  Text,
+  Box,
+  Flex,
+  Card,
+  Badge,
+  Button,
+} from "@radix-ui/themes";
+import { GitHubLogoIcon, ExternalLinkIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { projects } from "../../../../../data/projects";
+
+export async function generateStaticParams() {
+  return projects.map((project) => ({
+    params: { slug: project.slug },
+  }));
+}
+
+// Mock data (replace with actual data fetching logic)
+const getProjectData = (slug: String) =>
+  projects.find((project) => project.slug === slug);
+
+export default function ProjectPage({ params }: { params: { slug: string } }) {
+  const project = getProjectData(params.slug);
+
+  if (!project) {
+    return <Text size="5">Project not found</Text>;
+  }
+  return (
+    <Box className="container mx-auto px-4 py-8">
+      <Heading size="8" className="mb-6">
+        {project?.title}
+      </Heading>
+
+      <Flex direction="column" gap="6">
+        <Card>
+          <Flex direction="column" gap="4">
+            <Image
+              width={8000}
+              height={300}
+              src={project.image}
+              alt={project.title}
+              className="w-full h-auto rounded-lg "
+            />
+            <Text size="5">{project.description}</Text>
+            <Text as="p" size="3" className="text-gray-700">
+              {project.longDescription}
+            </Text>
+            <Flex wrap="wrap" gap="2">
+              {project.technologies.map((tech) => (
+                <Badge key={tech} variant="soft">
+                  {tech}
+                </Badge>
+              ))}
+            </Flex>
+            <Flex gap="4"></Flex>
+          </Flex>
+        </Card>
+
+        <Card>
+          <Heading size="6" className="mb-4">
+            Project Details
+          </Heading>
+          <Flex direction="column" gap="2">
+            <Text>
+              <strong>Started:</strong>{" "}
+              {project?.startDate?.toLocaleDateString("en-US", {
+                month: "long",
+                year: "numeric",
+              })}
+            </Text>
+            <Text>
+              <strong>Role:</strong> {project.myRole}
+            </Text>
+            {project.roleDescription?.split("\n").map((line) => (
+              <Text key={line}>{line}</Text>
+            ))}
+          </Flex>
+        </Card>
+      </Flex>
+    </Box>
+  );
+}
