d8bd5f77a51279876dfd18345c16344d126cad82
Steven Junio <Steven.junio91@gmail.com>
2024-07-13 17:46:22 -0700

    feat: Update contact page with Radix UI components and email link

--- apps/steven-junio/src/app/(frontend)/contact/page.tsx
+++ apps/steven-junio/src/app/(frontend)/contact/page.tsx
@@ -1,4 +1,6 @@
+import { Container, Flex, Heading, Text } from "@radix-ui/themes";
 import { Metadata } from "next";
+import Link from "next/link";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -28,39 +30,52 @@ export default function ContactPage() {
   }
 
   return (
-    <form
-      className="max-w-screen-md m-auto mt-4 bg-secondary p-5 flex flex-col gap-3"
-      action={handleContactForm}
-    >
-      <input
-        type="email"
-        placeholder="email"
-        required
-        title="email"
-        name="email"
-        className="bg-white h-10 p-4"
-      />
-      <input
-        type="text"
-        required
-        title="name"
-        name="name"
-        placeholder="name"
-        className="bg-white h-10 p-4"
-      />
-      <textarea
-        title="message"
-        name="message"
-        placeholder="message"
-        className="bg-white h-40 p-4"
-        minLength={5}
-      />
-      <button
-        type="submit"
-        className="bg-black text-secondary py-2 rounded mt-3"
+    <Container height={"calc(100vh - 70px)"}>
+      <Flex
+        direction="column"
+        gap="4"
+        height={"calc(100% )"}
+        justify={"center"}
+        align={"center"}
       >
-        Submit
-      </button>
-    </form>
+        <Link href={"mailto:steven.Junio91@gmail.com"}>
+          <Heading size="8">Steven.Junio91@gmail.com</Heading>
+        </Link>
+      </Flex>
+    </Container>
+    // <form
+    //   className="max-w-screen-md m-auto mt-4 bg-secondary p-5 flex flex-col gap-3"
+    //   action={handleContactForm}
+    // >
+    //   <input
+    //     type="email"
+    //     placeholder="email"
+    //     required
+    //     title="email"
+    //     name="email"
+    //     className="bg-white h-10 p-4"
+    //   />
+    //   <input
+    //     type="text"
+    //     required
+    //     title="name"
+    //     name="name"
+    //     placeholder="name"
+    //     className="bg-white h-10 p-4"
+    //   />
+    //   <textarea
+    //     title="message"
+    //     name="message"
+    //     placeholder="message"
+    //     className="bg-white h-40 p-4"
+    //     minLength={5}
+    //   />
+    //   <button
+    //     type="submit"
+    //     className="bg-black text-secondary py-2 rounded mt-3"
+    //   >
+    //     Submit
+    //   </button>
+    // </form>
   );
 }
--- apps/steven-junio/src/app/page.tsx
+++ apps/steven-junio/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
-import HideableHeader from "./components/HideableHeader";
 import TimeOfDayScene from "./components/TimeOfDayScene";
 import Link from "next/link";
+import { HoverCard } from "@radix-ui/themes";
 
 export default function Home() {
   return (
@@ -18,13 +18,14 @@ export default function Home() {
           <div className="z-10 px-6 items-center flex flex-col">
             <h1 className="w-fit text-5xl font-sans inset-0 bg-black bg-opacity-30 backdrop-blur-sm rounded p-3 text-white tracking-wide ">{`Hello, I'm Steven`}</h1>
             <h2 className="w-fit text-2xl bg-black bg-opacity-30 p-3 rounded text-white mt-3">
-              I develop performant, scalable software
+              I develop performant, scalable software web apps
             </h2>
             {/* Buttons container */}
             <div className="flex space-x-4 mt-4 justify-center">
               <Link href="/projects">
                 <Button variant={"default"}>View Projects</Button>
               </Link>
+
               <Link href={"/contact"}>
                 <Button variant={"secondary"}>Contact Me</Button>
               </Link>
